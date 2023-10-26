ca59ed773f2061f1dbce9df2f35b86555c886502
mams <mamadn21@gmail.com>
2023-10-26 20:20:16 +0200

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Coach & Clean',
+  title: 'Coach & Cleen',
   description: 'Coach professionnelle certifié',
 }
 
